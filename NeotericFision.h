a24d2908d555bd6f4d0462d650484687a3bca0dd
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2024-09-26 23:58:59 -0300

    Start neotic

---
+++ NeotericFision.h
@@ -0,0 +1,76 @@
+#ifndef _NEOTERIC_FISSION_H_
+#define _NEOTERIC_FISSION_H_
+#include <xtensor/xtensor.hpp>
+#include <string>
+
+namespace Fission {
+  using Coords = std::vector<std::tuple<int, int, int>>;
+
+  constexpr int neutronReach(4);
+  constexpr double modPower(1.0), modHeat(2.0);
+
+  enum {
+    // Cooler
+    Water, Redstone, Quartz, Gold, Glowstone,
+    Lapis, Diamond, Helium, Enderium, Cryotheum,
+    Iron, Emerald, Copper, Tin, Magnesium, Slime,
+    Netherite, Prismarine, Obsidian, Purpur, Fluorite,
+    Aluminum, Nether_Brick, Lithium, Arsenic, End_stone,
+    Manganese, Lead, Boron, Silver, Nitrogen, Active,
+    // Other
+    Cell = Active * 2, Moderator, Air
+  };
+
+  enum {
+    GoalPower,
+    GoalBreeder,
+    GoalEfficiency
+  };
+
+  struct Settings {
+    int sizeX, sizeY, sizeZ;
+    double fuelBasePower, fuelBaseHeat;
+    int limit[Air];
+    double coolingRates[Cell];
+    bool ensureActiveCoolerAccessible;
+    bool ensureHeatNeutral;
+    int goal;
+    bool symX, symY, symZ;
+  };
+
+  struct Evaluation {
+    // Raw
+    Coords invalidTiles;
+    double powerMult, heatMult, cooling;
+    int breed;
+    // Computed
+    double heat, netHeat, dutyCycle, avgMult, power, avgPower, avgBreed, efficiency;
+
+    void compute(const Settings &settings);
+  };
+
+  class Evaluator {
+    const Settings &settings;
+    xt::xtensor<int, 3> mults, rules;
+    xt::xtensor<bool, 3> isActive, isModeratorInLine, visited;
+    const xt::xtensor<int, 3> *state;
+    int compatibleTile;
+
+    int getTileSafe(int x, int y, int z) const;
+    int getMultSafe(int x, int y, int z) const;
+    bool countMult(int x, int y, int z, int dx, int dy, int dz);
+    int countMult(int x, int y, int z);
+    bool isActiveSafe(int tile, int x, int y, int z) const;
+    int countActiveNeighbors(int tile, int x, int y, int z) const;
+    bool isTileSafe(int tile, int x, int y, int z) const;
+    int countNeighbors(int tile, int x, int y, int z) const;
+    int countCasingNeighbors(int x, int y, int z) const;
+    bool checkAccessibility(int compatibleTile, int x, int y, int z);
+    bool checkAccessibility(int x, int y, int z);
+  public:
+    Evaluator(const Settings &settings);
+    void run(const xt::xtensor<int, 3> &state, Evaluation &result);
+  };
+}
+
+#endif
