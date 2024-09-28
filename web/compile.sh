dc86a7aa4f8ba5aca6063341f6e1f08e0d1db6ee
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2024-09-28 16:19:13 -0300

    Add compile for bash

---
+++ web/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+em++ --bind -s MODULARIZE=1 -s EXPORT_NAME=FissionOpt -s ALLOW_MEMORY_GROWTH=1 -o FissionOpt.js -std=c++17 -flto -O3 Bindings.cpp ../Fission.cpp ../OptFission.cpp ../FissionNet.cpp ../OverhaulFission.cpp ../OptOverhaulFission.cpp ../OverhaulFissionNet.cpp ../NeotericFision.cpp ../NeotericFisionNet.cpp ../OptNeotericFision.cpp -I../../xtl/include -I../../xtensor/include
